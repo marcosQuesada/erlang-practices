55945d73ff1ed55c8b974d50db8d16f0545b624b
marcosQuesada <marcos.quesadas@gmail.com>
2013-02-17 23:06:50 +0100

    clean out List

--- myRing.erl
+++ myRing.erl
@@ -1,27 +1,24 @@
 -module(myRing).
--export([start/2, node/2, addNode/4]).
+-export([start/2, node/2, addNode/3]).
 
-start(TotalNodes, Vueltas) ->
-    Steps = TotalNodes*Vueltas,
+start(TotalNodes, Loops) ->
+    Steps = TotalNodes*Loops,
     addNode(TotalNodes, Steps).
 
 %Adds first ring node
 addNode(TotalNodes, Steps) ->
     NewPid = spawn(myRing, node, [self(), []]),
-    addNode(TotalNodes, NewPid, [NewPid], Steps).
+    addNode(TotalNodes, NewPid, Steps).
 
 %Adds last ring node
-addNode(1, Pid, ListNodes, Steps)->
-    io:format("List completed, all: ~p~n", [ListNodes]),
+addNode(1, Pid, Steps)->
     Pid ! {message, Steps},
     node(Pid, []);
 
 %Iterates adding nodes except first and last
-addNode(TotalNodes, Pid, ListNodes, Steps)->
-    io:format("Adding Node ~p~n", [TotalNodes]),
+addNode(TotalNodes, Pid, Steps)->
     NewPid = spawn(myRing, node, [Pid, []]),
-    NewListNodes = [NewPid | ListNodes],
-    addNode(TotalNodes-1, NewPid, NewListNodes, Steps).
+    addNode(TotalNodes-1, NewPid, Steps).
 
 %Node Item
 %OriginPid : Process Origin reference
@@ -30,6 +27,7 @@ node(OriginPid, List) ->
         {message, 0} ->
             io:format("Exercise finished ~n", []),
             exit(normal);
+
         {message, Steps} ->
             NewSteps = Steps - 1,
             OriginPid ! {message, NewSteps},
