85a4988c4dd5fe48cbed67f18a428740ec3dd637
marcosQuesada <marcos.quesadas@gmail.com>
2013-02-18 11:12:14 +0000

    cleaning out node method by nodeFoo, avoided colision with erlang:node

--- myRing.erl
+++ myRing.erl
@@ -1,5 +1,5 @@
 -module(myRing).
--export([start/2, node/2, addNode/3]).
+-export([start/2, nodeFoo/1, addNode/3]).
 
 start(TotalNodes, Loops) ->
     Steps = TotalNodes*Loops,
@@ -7,22 +7,22 @@ start(TotalNodes, Loops) ->
 
 %Adds first ring node
 addNode(TotalNodes, Steps) ->
-    NewPid = spawn(myRing, node, [self(), []]),
+    NewPid = spawn(myRing, nodeFoo, [self()]),
     addNode(TotalNodes, NewPid, Steps).
 
 %Adds last ring node
 addNode(1, Pid, Steps)->
     Pid ! {message, Steps},
-    node(Pid, []);
+    nodeFoo(Pid);
 
 %Iterates adding nodes except first and last
 addNode(TotalNodes, Pid, Steps)->
-    NewPid = spawn(myRing, node, [Pid, []]),
+    NewPid = spawn(myRing, nodeFoo, [Pid]),
     addNode(TotalNodes-1, NewPid, Steps).
 
 %Node Item
 %OriginPid : Process Origin reference
-node(OriginPid, List) ->
+nodeFoo(OriginPid) ->
     receive
         {message, 0} ->
             io:format("Exercise finished ~n", []),
@@ -32,5 +32,5 @@ node(OriginPid, List) ->
             NewSteps = Steps - 1,
             OriginPid ! {message, NewSteps},
             io:format("Sended message from ~p to: ~p ~n", [self(), OriginPid]),
-            node(OriginPid, List)
+            nodeFoo(OriginPid)
     end.
